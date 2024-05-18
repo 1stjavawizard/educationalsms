cea344dec0eec50a922451108fa9bb658fc32d3c
Segun Sowemimo <segun2346@gmail.com>
2024-05-18 15:59:55 +0100

    updating the run.py

--- run.py
+++ run.py
@@ -1,12 +1,3 @@
-# ========================================================================== #
-#                           Junior School and College
-#                    A School Management System with FLASK
-#
-#       Create by some intelligent programmer
-#       Full CRUD facility
-#
-#       Github: https://github.com/mr-teslaa/Junior_School_and_College
-# ========================================================================== #
 
 from main_app import create_app
 
